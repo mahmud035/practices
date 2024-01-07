772d64d43669655cf3eae3f8bce50a4e2d63ec62
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-01-07 09:54:14 +0600

    customReact: how react compile JSX into javaScript Object behind the scene

---
+++ React JS/YouTube/Chai aur code/Chai aur react/customReact/custom-react.js
@@ -0,0 +1,41 @@
+function customRender(reactElement, mainContainer) {
+  /*   
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerText = reactElement.children;
+  domElement.setAttribute('href', reactElement.props.href);
+  domElement.setAttribute('target', reactElement.props.target);
+  mainContainer.appendChild(domElement);
+  */
+
+  // More dynamic way
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerText = reactElement.children;
+
+  for (const prop in reactElement.props) {
+    domElement.setAttribute(prop, reactElement.props[prop]);
+  }
+  mainContainer.appendChild(domElement);
+}
+
+const reactElement = {
+  type: 'a',
+  props: {
+    href: 'https://www.google.com/',
+    target: '_blank',
+  },
+  children: 'Click me to visit google',
+};
+
+const anotherReactElement = {
+  type: 'img',
+  props: {
+    src: 'https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    alt: 'Image of a tree & sun',
+    width: '600px',
+  },
+};
+
+const mainContainer = document.getElementById('root');
+
+customRender(reactElement, mainContainer);
+customRender(anotherReactElement, mainContainer);
---
+++ React JS/YouTube/Chai aur code/Chai aur react/customReact/how-bable-compile.js
@@ -0,0 +1,33 @@
+/* 
+
+<div id="root"> 
+
+  <a href="https://www.google.com/" target="_blank"> Click me to visit google </a> 
+  
+  <img src="https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg?auto=compress&amp;cs=tinysrgb&amp;w=1260&amp;h=750&amp;dpr=1" alt="Image of a tree &amp; sun" width="600px"/>
+
+</div> 
+
+*/
+
+//* NOTE: How Bable compiler the above reactElement/JSX into raw javaScript object
+
+/*#__PURE__*/ React.createElement(
+  'div',
+  {
+    id: 'root',
+  },
+  /*#__PURE__*/ React.createElement(
+    'a',
+    {
+      href: 'https://www.google.com/',
+      target: '_blank',
+    },
+    ' Click me to visit google '
+  ),
+  /*#__PURE__*/ React.createElement('img', {
+    src: 'https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    alt: 'Image of a tree & sun',
+    width: '600px',
+  })
+);
