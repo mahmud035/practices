8550d79847b67f2599857efff76b4d653ffb4d9f
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-07-24 18:51:16 +0600

    JavaScript Refresher - Function

--- JavaScript/Youtube/Learn With Sumit/JavaScript Refresher/function.js
+++ JavaScript/Youtube/Learn With Sumit/JavaScript Refresher/function.js
@@ -1,64 +1,87 @@
-//* regular function
-// function hello() {
-//   console.log('Hello World');
-// }
-
-// let msg = hello();
-// console.log(msg);
-
-//* function expression
-// const hello = function () {
-//   console.log('Hello World');
-// };
-
-//* named function expression
-// const hello = function hello() {
-//   console.log('Hello World');
-// };
-
-//* arrow function
-// const hello = () => console.log('Hello World');
-
-// const add = (a, b) => a + b;
-
-// IMPORTANT: When we want to return something from the arrow function, there are two ways to do it.
-// Suppose we want to return an object like that:
-/* 
-
+//* Function Declaration
 {
-  a: b,
-  b: 6
+  function hello() {
+    console.log('Hello World');
+  }
+
+  let msg = hello();
+  console.log(msg);
 }
 
-*/
+//* Function Expression
+{
+  const hello = function () {
+    console.log('Hello World');
+  };
 
-//? First way: (use parenthesis or () first brackets)
-const returnSomething = () => ({
-  a: 5,
-  b: 6,
-});
+  hello();
+}
 
-console.log(returnSomething()); // {a: 5, b: 6}
+//* Named Function Expression
+{
+  const hello = function hello() {
+    console.log('Hello World');
+  };
 
-//? Second way: use explicit return keyword
-const returnSomethingTwo = () => {
-  return {
+  hello();
+}
+
+//* Arrow Function
+{
+  const hello = () => console.log('Hello World');
+
+  hello();
+
+  const add = (a, b) => a + b; // implicit return
+
+  //* IMPORTANT: When we want to return something from the arrow function, there are Two ways to do it.
+  //* Suppose we want to return an object like that:
+
+  /* 
+    {
+      a: 5,
+      b: 6
+    }
+  */
+
+  //? First way: (use parenthesis or () first brackets)
+  const returnSomething = () => ({
     a: 5,
     b: 6,
+  });
+
+  console.log(returnSomething()); // {a: 5, b: 6}
+
+  //? Second way: use explicit return keyword
+  const returnSomethingTwo = () => {
+    return {
+      a: 5,
+      b: 6,
+    };
   };
-};
 
-console.log(returnSomethingTwo()); // {a: 5, b: 6}
+  console.log(returnSomethingTwo()); // {a: 5, b: 6}
+}
 
-//* anonymous function
-// function hello() {
-//   return () => {
-//     console.log('Hello World');
-//   };
-// }
+//* Anonymous Function
+// Ex: 1
+{
+  function hello() {
+    return () => {
+      console.log('Hello World');
+    };
+  }
 
-// function hello() {
-//   return function () {
-//     console.log('Hello World');
-//   };
-// }
+  hello()();
+}
+
+// Ex: 2
+{
+  function hello() {
+    return function () {
+      console.log('Hello World');
+    };
+  }
+
+  hello()();
+}
