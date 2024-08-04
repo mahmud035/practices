133384c1e03f42e995cc15e71b521f8b0bef8b04
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-08-05 00:11:18 +0600

    Solved 3 more problems on codewars

--- JavaScript/Problem Solving/Codewars Problem Solving/script.js
+++ JavaScript/Problem Solving/Codewars Problem Solving/script.js
@@ -31,3 +31,67 @@
   }
   // console.log(sumOfDifferences([10, 2, 1]));
 }
+
+// If you can't sleep, just count sheep!!
+//* My Solution
+{
+  const countSheep = function (num) {
+    const strArr = [];
+
+    for (let i = 0; i < num; i++) {
+      strArr.push(`${i + 1} sheep...`);
+    }
+
+    return strArr.join('');
+  };
+
+  // console.log(countSheep(3));
+}
+
+//* Best Practice
+{
+  const countSheep = function (num) {
+    let str = '';
+
+    for (let i = 1; i <= num; i++) {
+      str += `${i} sheep...`;
+    }
+
+    return str;
+  };
+
+  // console.log(countSheep(3));
+}
+
+// The Feast of Many Beasts
+//* My Solution
+{
+  function feast(beast, dish) {
+    return beast.at(0) === dish.at(0) && beast.at(-1) === dish.at(-1)
+      ? true
+      : false;
+  }
+
+  // console.log(feast('great blue heron', 'garlic naan'));
+  // console.log(feast('chickadee', 'chocolate cake'));
+}
+
+// Total amount of points
+//* My Solution
+{
+  function points(games) {
+    let totalPoints = 0;
+
+    games.forEach((game) => {
+      const x = Number.parseInt(game.at(0));
+      const y = Number.parseInt(game.at(-1));
+
+      if (x > y) totalPoints += 3;
+      if (x === y) totalPoints += 1;
+    });
+
+    return totalPoints;
+  }
+
+  // console.log(points(['3:1', '2:2', '0:1']));
+}
