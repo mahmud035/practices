5275a0e07df4ffabda378b17a8012beda0e0dbbd
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-08-04 23:03:18 +0600

    Solved problem: Sum of differences in array

---
+++ JavaScript/Problem Solving/Codewars Problem Solving/script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// Sum of differences in array
+//* My Solution
+{
+  function sumOfDifferences(arr) {
+    if (arr.length === 0 || arr.length === 1) return 0;
+
+    const sortedArray = arr.sort((a, b) => b - a);
+    let sum = 0;
+
+    sortedArray.forEach((element, index, array) => {
+      const firstElement = element;
+      const secondElement = array[index + 1];
+
+      if (secondElement !== undefined) {
+        const differences = firstElement - secondElement;
+        sum += differences;
+      }
+    });
+
+    return sum;
+  }
+  // console.log(sumOfDifferences([10, 2, 1]));
+}
+
+//* Clever Solution
+{
+  function sumOfDifferences(arr) {
+    return arr.length > 1 ? Math.max(...arr) - Math.min(...arr) : 0;
+  }
+  // console.log(sumOfDifferences([10, 2, 1]));
+}
