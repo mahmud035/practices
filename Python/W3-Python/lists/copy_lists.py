eab8e1ceed8cd0e0d8fd67a114a30db8e8975ad2
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-10-05 13:58:05 +0600

    Python - Copy Lists

---
+++ Python/W3-Python/lists/copy_lists.py
@@ -0,0 +1,30 @@
+# Python - Copy Lists:
+
+# Copy a List:
+
+# 1. Use the `copy()` method:
+# You can use the built-in List method `copy()` to copy a list.
+
+# Ex:
+# Make a copy of a list with the `copy()` method:
+fruits = ["apple", "banana", "cherry"]
+fruitsCopied = fruits.copy()
+print(fruitsCopied)
+
+# 2. Use the `list()` method:
+# Another way to make a copy is to use the built-in method `list()`.
+
+# Ex:
+# Make a copy of a list with the `list()` method:
+fruits = ["apple", "banana", "cherry"]
+fruitsCopied = list(fruits)
+print(fruitsCopied)
+
+# 3. Use the slice Operator:
+# You can also make a copy of a list by using the `:`(slice) operator.
+
+# Ex:
+# Make a copy of a list with the `:` operator:
+fruits = ["apple", "banana", "cherry"]
+fruitsCopied = fruits[:]
+print(fruitsCopied)
