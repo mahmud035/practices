a805a9389bde181a0d9a0979f4a9b4f67ae4f693
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-10-06 18:42:13 +0600

    Python Dictionary Methods

---
+++ Python/W3-Python/dictionaries/dictionary_methods.py
@@ -0,0 +1,14 @@
+# Python Dictionary Methods:
+
+# Dictionary Methods:
+# Python has a set of built-in methods that you can use on dictionaries.
+
+# `clear()`   => Removes all the elements from the dictionary
+# `copy()`    => Returns a copy of the dictionary
+# `formKeys()`=> Returns a dictionary with the specified keys and value
+# `get()`     => Returns the value of the specified key
+# `items()`   => Returns a list containing a tuple for each key value pair
+# `keys()`    => Returns a list containing the dictionary's keys
+# `values()`  => Returns a list of all the values in the dictionary
+# `pop()`     => Removes the element with the specified key
+# `update()`  => Updates the dictionary with the specified key-value pairs
