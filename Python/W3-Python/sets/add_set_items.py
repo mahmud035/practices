edebb7663707df5c057d609e9ef596af6ae8c7d1
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-10-06 15:05:58 +0600

    Python - Add Set Items

--- Python/W3-Python/sets/add_set_items.py
+++ Python/W3-Python/sets/add_set_items.py
@@ -13,3 +13,27 @@ fruits = {"apple", "banana", "cherry"}
 fruits.add("orange")
 
 print(fruits)
+
+# Add Sets:
+# To add items from another set into current set, use the `update()` method.
+
+# Ex:
+# Add elements from "tropical" into "fruits":
+fruits = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+
+fruits.update(tropical)
+
+print(fruits)
+
+# Add Any Iterable:
+# The object in the `update()` method does not have to be a set, it can be any iterable object (tuples, lists, dictionaries etc.).
+
+# Ex:
+# Add elements of a list to at set:
+fruits = {"apple", "banana", "cherry"}
+myList = ["kiwi", "orange"]
+
+fruits.update(myList)
+
+print(fruits)
