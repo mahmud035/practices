eb58746ddb0da25c389df4d7caa6a5790ed2f107
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-10-06 16:07:48 +0600

    Python - Set Methods

---
+++ Python/W3-Python/sets/set_methods.py
@@ -0,0 +1,8 @@
+# Python - Set Methods:
+
+# Set Methods:
+# Python has a set of built-in methods that you can use on sets.
+
+# `add()`   => Adds an element to the set
+# `clear()` => Removes all the elements from the set
+# `copy()`  => Returns a copy of the set
