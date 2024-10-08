2f6002a7c9891f41b82cfd6a2d477de3d58cf026
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-10-08 19:33:56 +0600

    TypeScript Introduction

--- TypeScript/TypeScript Book/dist/5. typescript-introduction.js
+++ TypeScript/TypeScript Book/dist/5. typescript-introduction.js
@@ -1,4 +1,60 @@
 "use strict";
 {
-    console.log('Introduction to TypeScript');
+    const sum = (a, b) => a + b;
+    console.log(sum(5, 10));
+    // To recognize types at runtime, we need to use another mechanism. TypeScript provides several options, with a common one being “tagged union”. For example:
+    {
+        const dog = {
+            kind: 'dog',
+            bark: () => console.log('bark'),
+        };
+        const cat = {
+            kind: 'cat',
+            meow: () => console.log('meow'),
+        };
+        const makeNoise = (animal) => {
+            if (animal.kind === 'dog') {
+                animal.bark();
+            }
+            else {
+                animal.meow();
+            }
+        };
+        makeNoise(dog);
+        makeNoise(cat);
+        // NOTE: The property “kind” is a value that can be used at runtime to distinguish between objects in JavaScript.
+    }
+    // TypeScript is a superset of JavaScript, so the “class” keyword can be used as a type and value at runtime.
+    {
+        class Animal {
+            constructor(name) {
+                this.name = name;
+            }
+        }
+        class Dog extends Animal {
+            constructor(name, bark) {
+                super(name);
+                this.name = name;
+                this.bark = bark;
+            }
+        }
+        class Cat extends Animal {
+            constructor(name, meow) {
+                super(name);
+                this.name = name;
+                this.meow = meow;
+            }
+        }
+        const dog = new Dog('Fido', () => console.log('bark'));
+        const makeNoise = (mammal) => {
+            if (mammal instanceof Dog) {
+                mammal.bark();
+            }
+            else {
+                mammal.meow();
+            }
+        };
+        makeNoise(dog);
+        // In JavaScript, a “class” has a “prototype” property, and the “instanceof” operator can be used to test if the prototype property of a constructor appears anywhere in the prototype chain of an object.
+    }
 }
--- TypeScript/TypeScript Book/src/5. typescript-introduction.ts
+++ TypeScript/TypeScript Book/src/5. typescript-introduction.ts
@@ -1,5 +1,79 @@
 {
-  console.log('Introduction to TypeScript');
-
   const sum = (a: number, b: number): number => a + b;
+
+  console.log(sum(5, 10));
+
+  // To recognize types at runtime, we need to use another mechanism. TypeScript provides several options, with a common one being “tagged union”. For example:
+
+  {
+    interface Dog {
+      kind: 'dog'; // Tagged union
+      bark: () => void;
+    }
+
+    interface Cat {
+      kind: 'cat'; // Tagged union
+      meow: () => void;
+    }
+
+    type Animal = Dog | Cat;
+
+    const dog: Dog = {
+      kind: 'dog',
+      bark: () => console.log('bark'),
+    };
+
+    const cat: Cat = {
+      kind: 'cat',
+      meow: () => console.log('meow'),
+    };
+
+    const makeNoise = (animal: Animal) => {
+      if (animal.kind === 'dog') {
+        animal.bark();
+      } else {
+        animal.meow();
+      }
+    };
+
+    makeNoise(dog);
+    makeNoise(cat);
+
+    // NOTE: The property “kind” is a value that can be used at runtime to distinguish between objects in JavaScript.
+  }
+
+  // TypeScript is a superset of JavaScript, so the “class” keyword can be used as a type and value at runtime.
+
+  {
+    class Animal {
+      constructor(public name: string) {}
+    }
+
+    class Dog extends Animal {
+      constructor(public name: string, public bark: () => void) {
+        super(name);
+      }
+    }
+
+    class Cat extends Animal {
+      constructor(public name: string, public meow: () => void) {
+        super(name);
+      }
+    }
+
+    type Mammal = Dog | Cat;
+
+    const dog = new Dog('Fido', () => console.log('bark'));
+
+    const makeNoise = (mammal: Mammal) => {
+      if (mammal instanceof Dog) {
+        mammal.bark();
+      } else {
+        mammal.meow();
+      }
+    };
+    makeNoise(dog);
+
+    // In JavaScript, a “class” has a “prototype” property, and the “instanceof” operator can be used to test if the prototype property of a constructor appears anywhere in the prototype chain of an object.
+  }
 }
