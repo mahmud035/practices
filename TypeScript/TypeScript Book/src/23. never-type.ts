867238386435f7472fafaae039a63d058a39c453
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-10-09 22:03:49 +0600

    The never Type

---
+++ TypeScript/TypeScript Book/src/23. never-type.ts
@@ -0,0 +1,18 @@
+{
+  // The never Type
+
+  // When a variable is narrowed to a type that cannot contain a any values, the TypeScript compiler will infer that the variable must be of the `never` type. This is because the "`never` type represents a value that can never be produced".
+
+  const printValue = (value: string | number) => {
+    if (typeof value === 'string') {
+      console.log(value.toUpperCase());
+    } else if (typeof value === 'number') {
+      console.log(value.toFixed(2));
+    } else {
+      // NOTE: `value` has type `never` here because it can never be anything other than a `string` or a `number`.
+
+      const neverValue: never = value;
+      console.log(`Unexpected value: ${neverValue}`);
+    }
+  };
+}
