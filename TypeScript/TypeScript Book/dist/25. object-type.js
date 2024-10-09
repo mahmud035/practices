2ac9c32ecbcb2f0bc8dc905ac62aa14081fc5f23
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-10-09 22:25:00 +0600

    Object Types

---
+++ TypeScript/TypeScript Book/dist/25. object-type.js
@@ -0,0 +1,6 @@
+"use strict";
+{
+    // It also possible to define a type anonymously:
+    const sum = (x) => x.a + x.b;
+    console.log(sum({ a: 5, b: 1 }));
+}
---
+++ TypeScript/TypeScript Book/src/25. object-type.ts
@@ -0,0 +1,28 @@
+{
+  // Object Types
+
+  // In TypeScript, object types describe the shape of an object. They specify the names and types of the object’s properties, as well as whether those properties are required or optional.
+
+  // In TypeScript, you can define object types in two primary ways:
+
+  // Interface which defines the shape of an object by specifying the names, types, and optionality of its properties.
+
+  interface User {
+    name: string;
+    age: number;
+    email?: string;
+  }
+
+  // Type alias, similar to an interface, defines the shape of an object. However, it can also create a new custom type that is based on an existing type or a combination of existing types. This includes defining union types, intersection types, and other complex types.
+
+  type Point = {
+    x: number;
+    y: number;
+  };
+
+  // It also possible to define a type anonymously:
+
+  const sum = (x: { a: number; b: number }) => x.a + x.b;
+
+  console.log(sum({ a: 5, b: 1 }));
+}
