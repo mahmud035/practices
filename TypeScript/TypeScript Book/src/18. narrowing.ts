543b7e0a3e06e037b1be513298e1cfc2a4db7f4d
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-10-09 19:26:06 +0600

    Narrowing

---
+++ TypeScript/TypeScript Book/src/18. narrowing.ts
@@ -0,0 +1,106 @@
+{
+  // Narrowing
+
+  // TypeScript narrowing is the process of refining the type of a variable within a conditional block.
+
+  // ✅ "This is useful when working with union types, where a variable can have more than one type".
+
+  // TypeScript recognizes several ways to narrow the type:
+
+  // 1. `typeof` operator
+
+  // ✅ Used to check the type of a value at runtime.
+
+  const fn = (x: number | string) => {
+    if (typeof x === 'number') {
+      return x + 1; // `x` is number
+    }
+    return -1;
+  };
+
+  // 2. Truthiness narrowing
+
+  // Truthiness narrowing in TypeScript works by checking weather a variable is truthy or falsy to narrow its type accordingly.
+
+  const toUpperCase = (name: string | null) => {
+    if (name) {
+      return name.toUpperCase();
+    } else {
+      return null;
+    }
+  };
+
+  // 3. Equality narrowing
+
+  // Equality narrowing in TypeScript works by checking weather a variable is equal to a specific value or NOT, to narrow its type accordingly.
+
+  // It is used in conjunction with `switch` statements and equality operators such as ===, !==, ==, and != to narrow down types.
+
+  const checkStatus = (status: 'success' | 'error') => {
+    switch (status) {
+      case 'success':
+        return true;
+      case 'error':
+        return null;
+    }
+  };
+
+  // 4. `in` Operator narrowing
+
+  // ✅ Used to check if a property exists in an object.
+
+  type Dog = {
+    name: string;
+    breed: string;
+  };
+
+  type Cat = {
+    name: string;
+    likesCream: boolean;
+  };
+
+  const getAnimalType = (pet: Dog | Cat) => {
+    if ('breed' in pet) {
+      return 'dog';
+    } else {
+      return 'cat';
+    }
+  };
+
+  // 5. `instanceof` narrowing
+
+  // ✅ Used to check if an object is an instance of a specific class.
+
+  class Square {
+    constructor(public width: number) {}
+  }
+
+  class Rectangle {
+    constructor(public width: number, public height: number) {}
+  }
+
+  function area(shape: Square | Rectangle) {
+    if (shape instanceof Square) {
+      return shape.width * shape.width;
+    } else {
+      return shape.width * shape.height;
+    }
+  }
+
+  const square = new Square(5);
+  const rectangle = new Rectangle(5, 10);
+
+  console.log(area(square)); // 25
+  console.log(area(rectangle)); // 50
+}
+
+// IMPORTANT: Narrow down types in TypeScript:
+/*
+  1. `instanceof` operator: Used to check if an object is an instance of a specific class.
+
+  2. `in` operator: Used to check if a property exists in an object.
+
+  3. `typeof` operator: Used to check the type of a value at runtime.
+
+  4. Built-in functions like `Array.isArray()`: Used to check if a value is an array.
+*/
