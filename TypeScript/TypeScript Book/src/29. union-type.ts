1ff74c7a11b7458e7b5bbea4f61a222d592a6639
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-10-10 09:30:18 +0600

    Union Type

---
+++ TypeScript/TypeScript Book/src/29. union-type.ts
@@ -0,0 +1,11 @@
+{
+  // Union Type
+
+  //  A Union Type is a type that represents a value that can be one of several types. Union Types are denoted using the `|` symbol between each possible type.
+
+  let x: string | number;
+
+  x = 'hello'; // Valid
+
+  x = 123; // Valid
+}
