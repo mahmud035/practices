5bbf41f1c5af7a1054d2ec9dfe76c90dec56f18f
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-10-10 10:48:21 +0600

    Mapped Type Modifiers

---
+++ TypeScript/TypeScript Book/src/36. mapped-type-modifiers.ts
@@ -0,0 +1,26 @@
+{
+  // Mapped Type Modifiers
+
+  // Mapped Type Modifiers in TypeScript enable the transformation of properties within an existing type:
+
+  /* 
+  👉 `readonly` or `+readonly`: This renders a property in the mapped type as read-only.
+
+  👉 `-readonly`: This allows a property in the mapped type to be mutable.
+
+  👉 `?`: This designates a property in the mapped type as optional.
+  */
+
+  // Ex:
+  type ReadOnly<T> = {
+    readonly [P in keyof T]: T[P]; // All properties marked as read-only
+  };
+
+  type Mutable<T> = {
+    -readonly [P in keyof T]: T[P]; // All properties marked as mutable
+  };
+
+  type MyPartial<T> = {
+    [P in keyof T]?: T[P]; // All properties marked as optional
+  };
+}
