122889ba5a7b3992aaed1e9b2d4ff78eaf8cd8ad
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-10-10 09:54:01 +0600

    Type from Value

---
+++ TypeScript/TypeScript Book/dist/32. type-from-value.js
@@ -0,0 +1,7 @@
+"use strict";
+{
+    //  Type from Value
+    // Type from Value in TypeScript refers to the "automatic inference of a type from a value of expression through type inference".
+    const x = 'x'; // type infer as 'x'
+    let y = 'y'; // type infer as 'string'
+}
