4e3f83edec7ca695e09152fc3c9ba28e9e3308d5
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-10-13 11:05:53 +0600

    Void type

---
+++ TypeScript/TypeScript Book/src/44. void-type.ts
@@ -0,0 +1,11 @@
+{
+  // Void type
+
+  // The `void` type is used to indicate that a function does not return a value.
+
+  const sayHello = (): void => {
+    console.log('Hello');
+  };
+
+  sayHello();
+}
