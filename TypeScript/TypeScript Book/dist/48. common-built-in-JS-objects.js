d8ce7a1bcc83ee953208262db45b73a60a02cac3
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-10-13 12:24:45 +0600

    Common Built-in JS Objects

---
+++ TypeScript/TypeScript Book/dist/48. common-built-in-JS-objects.js
@@ -0,0 +1,20 @@
+"use strict";
+// Common Built-in JS Objects
+// TypeScript is a superset of JavaScript, it includes all the commonly used built-in JavaScript objects.
+// NOTE: Here is a list of some commonly used built-in JavaScript objects:
+// Function
+// Object
+// Boolean
+// Error
+// Number
+// BigInt
+// Math
+// Date
+// String
+// RegExp
+// Array
+// Map
+// Set
+// Promise
+// Intl
+console.log('Common Built-in JS Objects');
