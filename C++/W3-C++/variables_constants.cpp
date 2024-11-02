883878fe40676595824304b33d3cea6adda8fc44
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-11-02 23:11:39 +0600

    C++ Constants

---
+++ C++/W3-C++/variables_constants.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  // Constants
+  // When you do not want others (or yourself) to change existing variable values, use the `const` keyword (this will declare the variable as "constant", which means unchangeable and read-only):
+
+  const int myNum = 15;  // `myNum` will always be 15
+
+  // NOTE: You should always declare the variable as constant when you have values that are unlikely to change:
+
+  const int minutesPerHour = 60;
+  const float PI = 3.14;
+
+  return 0;
+}
---
+++ C++/W3-C++/variables_identifiers.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  // All C++ variables must be identified with unique names.
+
+  // NOTE: It is recommended to use descriptive names in order to create understandable and maintainable code:
+
+  // ✅ Good
+  int minutesPerHour = 60;
+
+  // OK, but not so easy to understand what m actually is
+  int m = 60;
+
+  return 0;
+}
