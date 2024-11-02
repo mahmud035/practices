499e6da5f0a52f9a5aac959f3d3608fbb9a73423
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-11-02 23:21:10 +0600

    C++ Variables Examples

---
+++ C++/W3-C++/variables_real_life_example.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  // For a practical example of using variables, we have created a program that stores different data about a college student:
+
+  // Student data
+  int studentID = 15;
+  int studentAge = 23;
+  float studentFee = 75.25;
+  char studentGrade = 'B';
+
+  // Print variables
+  cout << "Student ID: " << studentID << endl;
+  cout << "Student Age: " << studentAge << endl;
+  cout << "Student Fee: " << studentFee << endl;
+  cout << "Student Grade: " << studentGrade << endl;
+
+  // Calculate the Area of a Rectangle
+  // In this real-life example, we create a program to calculate the area of a rectangle (by multiplying the length and width):
+
+  // Create integer variable
+  int length = 4;
+  int width = 6;
+  int area;
+
+  // Calculate the area of a rectangle
+  area = length * width;
+
+  // Print the variables
+  cout << "Length is: " << length << endl;
+  cout << "Width is: " << width << endl;
+  cout << "Area is: " << area << endl;
+
+  return 0;
+}
