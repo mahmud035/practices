513d56fafa1aca30d8d6293dd200448f4bf1a23f
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-11-02 23:29:17 +0600

    C++ user input

---
+++ C++/W3-C++/input.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  // int a;
+  // cin >> a;
+  // cout << a << endl;
+
+  // Creating a Simple Calculator
+  // In this example, the user must input two numbers. Then we print the sum by calculating (adding) the two numbers:
+
+  int x, y, sum;
+  cin >> x >> y;
+  sum = x + y;
+  cout << "Sum is: " << sum << endl;
+
+  return 0;
+}
