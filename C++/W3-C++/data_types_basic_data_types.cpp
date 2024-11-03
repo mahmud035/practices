81cf55b2679af114c5a3a027d988a94b86a99181
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-11-03 11:55:46 +0600

    C++ Basic Data Types

---
+++ C++/W3-C++/data_types_basic_data_types.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  // C++ Data Types
+  int myNum = 5;
+  float myFloatNum = 5.99;
+  double myDoubleNum = 9.894739374937;
+  char myLetter = 'A';
+  string myText = "Hello";
+  bool myBoolean = true;
+
+  return 0;
+}
