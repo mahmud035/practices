fb59e72b1bf626e7706da644cf6f0226a87199b4
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-11-06 14:32:01 +0600

    Added more Python code

---
+++ Python/Python basics/input.py
@@ -0,0 +1,21 @@
+# print("Now I need money")
+# input()
+
+# input("Give me some money: ")
+
+# money = input("Give me some money: ")
+# print(f"here is your money {money} tk")
+
+# IMPORTANT: By Default Input type will be String
+
+first_money = input("Kodom Ali, kichu taka dey: ")
+second_money = input("Peyara kichu taka dey: ")
+print(first_money, second_money)
+# print(type(first_money))
+# print(type(second_money))
+
+first_money_int = int(first_money)
+second_money_int = int(second_money)
+
+total = first_money_int + second_money_int
+print("Total money I got:", total)
---
+++ Python/Python basics/variable.py
@@ -0,0 +1,21 @@
+# Variable Declaration
+age = 28
+interest_rate = 2.5
+name = "mahmud"
+district = "gopalganj"
+is_raining = True
+
+print(age + interest_rate)
+print(district)
+
+# Variable Type Checking
+print(type(age))
+print(type(interest_rate))
+print(type(name))
+print(type(is_raining))
+
+print("Mahamudul" + " " + "Hasan")
+
+# Template Literals
+text = f"My name is {name}. I am {age} years old. I live in {district}."
+print(text)
