1bef4ad47fd0cdecbc6908bc6b78420fed54f24d
mahmud035 <mahamudulhasan4148@gmail.com>
2025-02-06 20:41:42 +0600

    TypeScript Dave Gray video complete

---
+++ TypeScript/TypeScript - Dave Gray/lesson17/src/components/Nav.tsx
@@ -0,0 +1,18 @@
+interface INavProps {
+  viewCart: boolean;
+  setViewCart: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Nav = ({ viewCart, setViewCart }: INavProps) => {
+  const button = viewCart ? (
+    <button onClick={() => setViewCart(false)}>View Products</button>
+  ) : (
+    <button onClick={() => setViewCart(true)}>View Cart</button>
+  );
+
+  const content = <nav className="nav">{button}</nav>;
+
+  return content;
+};
+
+export default Nav;
