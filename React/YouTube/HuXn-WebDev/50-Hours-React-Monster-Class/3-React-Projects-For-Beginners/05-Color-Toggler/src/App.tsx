b2cc515578628f5d8718c380ca9ac3522b0e3e39
mahmud035 <mahamudulhasan4148@gmail.com>
2025-02-24 16:58:12 +0600

    Complete 50 hours class - React project for beginners-project05 color-toggler

---
+++ React/YouTube/HuXn-WebDev/50-Hours-React-Monster-Class/3-React-Projects-For-Beginners/05-Color-Toggler/src/App.tsx
@@ -0,0 +1,9 @@
+import ToggleBackgroundColor from './components/ToggleBackgroundColor';
+
+export default function App() {
+  return (
+    <main>
+      <ToggleBackgroundColor />
+    </main>
+  );
+}
