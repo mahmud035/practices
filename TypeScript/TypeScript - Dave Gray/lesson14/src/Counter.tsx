2f8a720f2235e3b2917694f89babd2f2dd5201a9
mahmud035 <mahamudulhasan4148@gmail.com>
2025-02-25 16:19:25 +0600

    modify

--- TypeScript/TypeScript - Dave Gray/lesson14/src/Counter.tsx
+++ TypeScript/TypeScript - Dave Gray/lesson14/src/Counter.tsx
@@ -1,28 +1,35 @@
 import { useReducer } from 'react';
 
-type REDUCER_ACTION_TYPE = 'INCREMENT' | 'DECREMENT' | 'RESET';
+type State = { count: number };
 
-interface ReducerAction {
-  type: REDUCER_ACTION_TYPE;
+type REDUCER_ACTIONS_TYPE = 'INCREMENT' | 'DECREMENT' | 'RESET';
+
+interface IReducerAction {
+  type: REDUCER_ACTIONS_TYPE;
   payload?: number;
 }
 
-const reducer = (state: number, action: ReducerAction): number => {
+const counterReducer = (state: State, action: IReducerAction): State => {
   switch (action.type) {
-    case 'INCREMENT':
-      return state + (action.payload || 1);
-    case 'DECREMENT':
-      return state - (action.payload || 1);
-    case 'RESET':
-      return 0;
+    case 'INCREMENT': {
+      return { count: state.count + (action.payload ?? 1) };
+    }
+    case 'DECREMENT': {
+      return { count: state.count - (action.payload ?? 1) };
+    }
+    case 'RESET': {
+      return { count: 0 };
+    }
     default:
       throw new Error('Unknown action type');
   }
 };
 
+const initialState: State = { count: 0 };
+
 // Example usage in a component
-const Counter = () => {
-  const [count, dispatch] = useReducer(reducer, 0);
+export default function Counter() {
+  const [state, dispatch] = useReducer(counterReducer, initialState);
 
   const increment = () => dispatch({ type: 'INCREMENT', payload: 5 });
   const decrement = () => dispatch({ type: 'DECREMENT', payload: 3 });
@@ -30,13 +37,12 @@ const Counter = () => {
 
   return (
     <div>
-      <p>Count: {count}</p>
+      <h2>Reducer</h2>
+      <p>Count: {state.count}</p>
 
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
       <button onClick={reset}>Reset</button>
     </div>
   );
-};
-
-export default Counter;
+}
