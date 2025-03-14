429f4e5996d6d5a6f13c2f28fa6f58cca9179c5d
mahmud035 <mahamudulhasan4148@gmail.com>
2025-03-14 19:01:51 +0600

    modify

---
+++ Tailwind CSS/Daisy UI/src/components/Button.tsx
@@ -0,0 +1,11 @@
+export default function Button() {
+  return (
+    <div>
+      <button className="btn btn-primary">One</button>
+      <button className="btn btn-secondary">Two</button>
+      <button className="btn btn-accent btn-outline">Three</button>
+      <button className="btn rounded-full">Four</button>
+      <button className="btn rounded-none px-16">Five</button>
+    </div>
+  );
+}
