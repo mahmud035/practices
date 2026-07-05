49a78e26e907763d59f10f36fe5b18b18927e0fd
mahmud035 <mahamudulhasan4148@gmail.com>
2026-07-05 10:18:00 +0600

    add new example in react playground

---
+++ react/playground/src/examples/ai-responding-to-events/App.jsx
@@ -0,0 +1,11 @@
+import './style.css';
+import Toolbar from './ToolBar';
+
+export default function App() {
+  return (
+    <Toolbar
+      onPlayMovie={() => alert('Playing!')}
+      onUploadImage={() => alert('Uploading!')}
+    />
+  );
+}
